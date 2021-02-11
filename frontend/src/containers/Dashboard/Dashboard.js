81b5817957ab70f2f588339e4f373873273d73d9
bharti <bhartisawaria71@gmail.com>
2021-02-11 19:23:07 +0530

    Frontend/Added DM functionality, removed dummy data

--- frontend/src/containers/Dashboard/Dashboard.js
+++ frontend/src/containers/Dashboard/Dashboard.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import MenuBar from '../../components/MenuBar';
 import NavBar from '../../components/NavBar';
 import styles from './Dashboard.module.css';
-import ChatSection from '../../components/ChatSection'
+import ChatSection from '../../components/ChatSection';
+import SubscribersModal from '../../components/SubscribersModal/index';
 import openSocket from 'socket.io-client';
 import { connect } from 'react-redux';
 import * as actionCreators from '../../actions/index';
-import SubscribersModal from '../../components/SubscribersModal';
 
 class Dashboard extends Component{
+
+    state = {
+        showSubscribersModal: false
+    }
 
     componentDidMount(){
         // const socket = openSocket('http://localhost:8080');
@@ -21,28 +25,40 @@ class Dashboard extends Component{
         }
     }
 
+    subscribersModalShowHandler = () => {
+        this.setState({showSubscribersModal: true})
+    };
+
+    subscribersModalHideHandler = () => this.setState({showSubscribersModal: false});
+
     render(){
-        const { subscribedChannels, friends, channelOpened, dmOpened } = this.props;
+        const { user, subscribedChannels, friends, channelOpened, dmOpened, openChannel, openDm, directMessages, channelMessages } = this.props;
+        const { showSubscribersModal } = this.state;
         return(
             <div className={styles.dashboard_page}>
-                <MenuBar />
+                <MenuBar setLogout={this.props.setLogout}/>
                 <div className={styles.main_box}>
                     <NavBar 
                         subscribedChannels={subscribedChannels} 
                         friends={friends}
                         channelOpened={channelOpened}
                         dmOpened={dmOpened}/>
-                    <ChatSection />
+                    {(openChannel || openDm) ? (
+                        <ChatSection 
+                            user={user}
+                            openChannel={openChannel}
+                            openDm={openDm}
+                            directMessages={directMessages}
+                            channelMessages={channelMessages} />
+                        ) : null   }
+                    
                 </div>
                 
-                {/* TODO: add the modal useState here  */}
-                {/* <div className={styles.modals}> */}
-                    {/* <SubscribersModal /> */}
-                {/* </div> */}
-
-
-                {/* TODO: Button has been relocated to MenuBar */}
-                <button onClick={this.props.setLogout}>Logout</button>
+                {/* {showSubscribersModal ? (
+                    <div className={styles.modals}>
+                        <SubscribersModal onClick={this.subscribersModalShowHandler}/>
+                    </div>
+                ) : null}    */}
             </div>
         )
     }
@@ -50,8 +66,13 @@ class Dashboard extends Component{
 
 const mapStateToProps = state => {
 	return{
+        user: state.auth.user,
         friends: state.user.friends,
-        subscribedChannels: state.user.subscribedChannels
+        subscribedChannels: state.user.subscribedChannels,
+        openChannel: state.user.openChannel,
+        openDm: state.user.openDm,
+        directMessages: state.user.directMessages,
+        channelMessages: state.user.channelMessages
 	}
 }
 
