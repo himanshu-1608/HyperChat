d833c2603c34948eb34991ac9f44176a0f8390a3
Rahul-Yadav-00 <rahulyadav5524@gmail.com>
2021-02-12 12:36:25 +0530

    edit and delete option is added in message box

--- frontend/src/components/Message/index.js
+++ frontend/src/components/Message/index.js
@@ -1,5 +1,8 @@
 import { Component } from 'react';
 import styles from './Message.module.css';
+import { BiEdit } from 'react-icons/bi';
+import { AiOutlineDelete } from 'react-icons/ai';
+import { HiOutlineDotsVertical } from 'react-icons/hi';
 
 class Message extends Component {
     render() {
@@ -8,19 +11,28 @@ class Message extends Component {
             <div className={styles.message_box}>
                 {/* TODO: image url should be added here */}
                 <div className={styles.user_image}>
-                    <img src="" alt="User"/>
+                    <img src="" alt="User" />
                 </div>
                 <div className={styles.message_section}>
                     <div className={styles.message_info}>
                         <div className={styles.user_name}>
                             {senderID.userName}
                         </div>
-                        <div className={styles.time}>
-                            {sentTime}
-                        </div>
+                        <div className={styles.time}>{sentTime}</div>
                     </div>
-                    <div className={styles.message}>
-                        {messagePayload}
+                    <div className={styles.message}>{messagePayload}</div>
+                </div>
+                <div className={styles.message_options}>
+                    {/* TODO: edit option */}
+                    <div className={styles.option}>
+                        <BiEdit />
+                    </div>
+                    {/* TODO: delete option */}
+                    <div className={styles.option}>
+                        <AiOutlineDelete />
+                    </div>
+                    <div className={styles.option}>
+                        <HiOutlineDotsVertical />
                     </div>
                 </div>
             </div>
